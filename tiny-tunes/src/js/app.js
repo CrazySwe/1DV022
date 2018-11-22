94818554034fea2b5522a719773296df8171535c
Kevin C <crazy_swe_@hotmail.com>
2018-11-22 03:24:53 +0100

    Adding exercise 1 & exercise 2

--- tiny-tunes/src/js/app.js
+++ tiny-tunes/src/js/app.js
@@ -1,3 +1,6 @@
-'use strict'
+import * as exLib from './exercises.js'
 
-console.log('Hello! World!!')
+exLib.exercise1()
+exLib.exercise2()
+
+// console.log('Hello! World!!')
---
+++ tiny-tunes/src/js/exercises.js
@@ -0,0 +1,26 @@
+// 01
+export function exercise1 () {
+  let myTxt = document.createTextNode('Hello World!')
+  document.querySelector('#step01_hello').appendChild(myTxt)
+}
+// 02
+export function exercise2 () {
+  let myH2 = document.createElement('h2')
+  let myElmt = document.querySelector('#step02')
+  myH2.innerHTML = 'This is a sub headline'
+  myElmt.appendChild(myH2)
+}
+// 03
+export function exercise3 () {}
+// 04
+export function exercise4 () {}
+// 05
+export function exercise5 () {}
+// 06
+export function exercise6 () {}
+// 07
+export function exercise7 () {}
+// 08
+export function exercise8 () {}
+// 09
+export function exercise9 () {}
