17d7dc8913d94b4fd77ec4be082973a58215e594
Kevin C <crazy_swe_@hotmail.com>
2018-12-07 06:32:11 +0100

    adding final part 2  - bartboard

--- bartboard/src/js/app.js
+++ bartboard/src/js/app.js
@@ -6,7 +6,10 @@
 import './bart-board.js'
 
 let bb1 = document.createElement('bart-board')
-bb1.setAttribute('text', 'This is working really well')
+bb1.setAttribute('text', 'This is working really well! ')
+bb1.setAttribute('speed', '2')
 document.querySelector('#board').appendChild(bb1)
 
-bb1.setAttribute('text', 'Huuh?')
+bb1.addEventListener('filled', () => {
+  bb1.wipeBoard()
+})
--- bartboard/src/js/bart-board.js
+++ bartboard/src/js/bart-board.js
@@ -19,30 +19,69 @@ template.innerHTML = `
     float:left;
     border-radius: 3px;
   }
+  p {
+    margin:0;
+    padding:0;
+  }
 </style>
 
-<p id='text'>I love JS</p>
-
+<p id='text'></p>
 `
-
 export default class BartBoard extends window.HTMLElement {
   constructor () {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this._p = this.shadowRoot.querySelector('#text')
+    this._intervalID = null
+    this._letter = 0
+    this._text = 'Låt stå!'
+    this._speed = 50
   }
   static get observedAttributes () {
-    return ['text']
+    return ['text', 'speed']
   }
   attributeChangedCallback (name, oldValue, newValue) {
     if (name === 'text') {
-      this.shadowRoot.querySelector('#text').innerText = newValue
+      this._text = newValue
+    } else if (name === 'speed') {
+      this._speed = newValue
     }
   }
   connectedCallback () {
-    if (this.hasAttribute('text')) {
-      this.shadowRoot.querySelector('#text').innerText = this.getAttribute('text')
-    }
+    this.addEventListener('mousedown', this._onWrite)
+    this.addEventListener('mouseup', this.stopWriting)
+    this.addEventListener('mouseleave', this.stopWriting)
+  }
+
+  disconnectedCallback () {
+    this.removeEventListener('mousedown', this._onWrite)
+    this.removeEventListener('mouseup', this.stopWriting)
+    this.removeEventListener('mouseleave', this.stopWriting)
+    this.stopWriting()
+  }
+
+  _onWrite (event) {
+    this._intervalID = setInterval(() => {
+      if (this._p.offsetHeight >= this.offsetHeight) {
+        this.dispatchEvent(new window.CustomEvent('filled'))
+        this.stopWriting()
+        return
+      }
+      this._p.textContent += this._text.charAt(this._letter++)
+      if (this._letter >= this._text.length) {
+        this._p.textContent += ' '
+        this._letter = 0
+      }
+    }, this._speed)
+  }
+
+  stopWriting (event) {
+    clearTimeout(this._intervalID)
+  }
+  wipeBoard () {
+    this._p.textContent = ''
+    this._letter = 0
   }
 }
 window.customElements.define('bart-board', BartBoard)
