092c140c4a85489c4573c241a98eac296d4322d2
Kevin C <crazy_swe_@hotmail.com>
2019-01-03 06:03:10 +0100

    Adding base files and basic window

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * @author Kevin Cederholm
+ * @version 1.0
+ */
+
+import WebDesktop from './webdesktop.js'
+
+let webD = new WebDesktop()
+webD.run()
---
+++ src/js/webdesktop.js
@@ -0,0 +1,15 @@
+
+/**
+ * @author Kevin Cederholm
+ * @version 1.0
+ */
+export default class WebDesktop {
+  constructor () {
+    this.windows = []
+  }
+
+  run () {
+    // Starting the WebDesktop from here?
+    // add eventlisteners on the desktop
+  }
+}
