7cbd311a90c0e299f32ab685ef2535bb50e205c5
Kevin C <crazy_swe_@hotmail.com>
2019-01-04 12:58:19 +0100

    Adding styling

--- src/js/webdesktop.js
+++ src/js/webdesktop.js
@@ -11,8 +11,6 @@ export default class WebDesktop {
   constructor () {
     this.windows = []
     this.focus = null
-    this.x = null
-    this.y = null
     this.dragMouseFunc = this.onDragMouse.bind(this)
     this.mouseUpFunc = this.mouseUp.bind(this)
   }
@@ -20,6 +18,7 @@ export default class WebDesktop {
   run () {
     document.addEventListener('mousedown', this.mouseDown.bind(this))
     document.addEventListener('keydown', this.keyDown.bind(this))
+    window.addEventListener('resize', this.onDeskResize.bind(this))
     // Starting the WebDesktop from here?
     // add eventlisteners on the desktop
   }
@@ -27,6 +26,10 @@ export default class WebDesktop {
   mouseDown (event) {
     event.preventDefault()
     if (event.target.className === 'window-topbar') {
+      // viewport size for boundaries
+      // window.innerWidth
+      // window.innerHeigth
+      console.log(event.target.parentElement)
       this.focus = event.target
 
       document.addEventListener('mousemove', this.dragMouseFunc)
@@ -50,6 +53,17 @@ export default class WebDesktop {
     event.preventDefault()
     this.focus.parentElement.style.top = event.clientY + 'px'
     this.focus.parentElement.style.left = event.clientX + 'px'
-    // console.log('x ' + event.clientX + ': y ' + event.clientY)
+    console.log('new pos: x ' + event.clientX + ': y ' + event.clientY)
+  }
+
+  onDeskResize () {
+    /**
+     * foreach window
+     *  if outside boundaries
+     *    moveinsideboundaries
+     *  endif
+     * endforeach
+     */
+    console.log('Someone changed the size of the desk!')
   }
 }
