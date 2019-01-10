336e214a671b150f42bb3d88d9f5dcf7504308cc
Kevin C <crazy_swe_@hotmail.com>
2019-01-10 14:33:55 +0100

    Adding Point2d class and updating window class

--- src/js/webdesktop.js
+++ src/js/webdesktop.js
@@ -1,4 +1,5 @@
 import Window from './window.js'
+import Point2D from './point2d.js'
 /**
  * @author Kevin Cederholm
  * @version 1.0
@@ -13,40 +14,40 @@ export default class WebDesktop {
     this.zCount = 0
     this.idCount = 0
     this.windows = []
-    this.focusedWindowedWindow = null
-    this.offsetX = 0
-    this.offsetY = 0
-    this.dragMouseFunc = this.onDragMouse.bind(this)
-    this.mouseUpFunc = this.mouseUp.bind(this)
+    this.focusedWindow = null
+    this.offset = new Point2D(0, 0)
+    // this.offsetX = 0
+    // this.offsetY = 0
+    // this.dragMouseFunc = this.onDragMouse.bind(this)
+    // this.mouseUpFunc = this.mouseUp.bind(this)
   }
 
   run () {
     window.addEventListener('mousedown', this.mouseDown.bind(this))
-    window.addEventListener('keydown', this.keyDown.bind(this))
-    window.addEventListener('resize', this.onDeskResize.bind(this))
+    // window.addEventListener('keydown', this.keyDown.bind(this))
+    // window.addEventListener('resize', this.onDeskResize.bind(this))
 
     // Test window
-    let newWindow = new Window(this.idCount++, 'TestWindow', 'test', this.zCount++)
-    this.windows.push(newWindow)
+    // let newWindow = new Window(this.idCount++, 'TestWindow', 'test', this.zCount++)
+    // this.windows.push(newWindow)
 
-    // console.log(this.windows)
     // Starting the WebDesktop from here?
     // add eventlisteners on the desktop
   }
 
   mouseDown (event) {
-    event.preventDefault()
-
-    // window.element.contains(event.target)
+    // event.preventDefault()
     // Identify window correctly here
     if (event.target.className === 'window-topbar') {
-      this.offsetY = (event.clientY - parseInt(event.target.parentElement.style.top, 10))
-      this.offsetX = (event.clientX - parseInt(event.target.parentElement.style.left, 10))
+      // this.offset.x = (event.clientX - parseInt(event.target.parentElement.style.left, 10))
+      // this.offset.y = (event.clientY - parseInt(event.target.parentElement.style.top, 10))
+      // this.offsetY = (event.clientY - parseInt(event.target.parentElement.style.top, 10))
+      // this.offsetX = (event.clientX - parseInt(event.target.parentElement.style.left, 10))
+      console.log('this is running also')
+      // this.setfocus(event.target)
 
-      this.setfocus(event.target)
-
-      document.addEventListener('mousemove', this.dragMouseFunc)
-      document.addEventListener('mouseup', this.mouseUpFunc)
+      // document.addEventListener('mousemove', this.dragMouseFunc)
+      // document.addEventListener('mouseup', this.mouseUpFunc)
     } else if (event.target.tagName === 'LI') {
       switch (event.target.attributes.value.value) {
         case 'quizapp':
@@ -55,39 +56,42 @@ export default class WebDesktop {
         case 'memoryapp':
           this.createWindow('memoryappen', 'putcontenthere')
           break
+        case 'chatapp':
+          this.createWindow('ChatApp', 'Content for chat here')
+          break
       }
     }
   }
 
-  mouseUp (event) {
-    event.preventDefault()
-    document.removeEventListener('mousemove', this.dragMouseFunc)
-    document.removeEventListener('mouseup', this.mouseUpFunc)
-  }
+  // mouseUp (event) {
+  //   event.preventDefault()
+  //   document.removeEventListener('mousemove', this.dragMouseFunc)
+  //   document.removeEventListener('mouseup', this.mouseUpFunc)
+  // }
 
   keyDown (event) {
     // event.preventDefault()
     console.log(event.key)
   }
 
-  onDragMouse (event) {
-    event.preventDefault()
-    this.moveWindowTo((event.clientX - this.offsetX), (event.clientY - this.offsetY))
-    // console.log('new pos: x ' + event.clientX + ': y ' + event.clientY)
-  }
+  // onDragMouse (event) {
+  //   event.preventDefault()
+  //   this.moveWindowTo((event.clientX - this.offset.x), (event.clientY - this.offset.y))
+  //   // console.log('new pos: x ' + event.clientX + ': y ' + event.clientY)
+  // }
 
-  moveWindowTo (x, y) {
-    // Set maximum offscreen
-    let halfWidth = this.focusedWindow.parentElement.clientWidth / 2
-    let halfHeigth = this.focusedWindow.parentElement.clientWidth / 2
+  // moveWindowTo (x, y) {
+  //   // Set maximum offscreen
+  //   let halfWidth = this.focusedWindow.parentElement.clientWidth / 2
+  //   let halfHeigth = this.focusedWindow.parentElement.clientWidth / 2
 
-    if (x >= 0 && x <= (window.innerWidth - halfWidth)) {
-      this.focusedWindow.parentElement.style.left = x + 'px'
-    }
-    if (y >= 0 && y <= (window.innerHeight - halfHeigth)) {
-      this.focusedWindow.parentElement.style.top = y + 'px'
-    }
-  }
+  //   if (x >= 0 && x <= (window.innerWidth - halfWidth)) {
+  //     this.focusedWindow.parentElement.style.left = x + 'px'
+  //   }
+  //   if (y >= 0 && y <= (window.innerHeight - halfHeigth)) {
+  //     this.focusedWindow.parentElement.style.top = y + 'px'
+  //   }
+  // }
 
   onDeskResize () {
     /**
@@ -101,16 +105,13 @@ export default class WebDesktop {
   }
 
   setfocus (element) {
+    // remove class activewindow on old
     this.focusedWindow = element
+    // add class activewindow on new
     element.style.zIndex = this.zCount++
   }
 
   createWindow (name, content) {
-    let newWindow = new Window(this.idCount++, name, content, this.zCount++)
-    this.windows.push(newWindow)
-    this.desktopElement.append(newWindow.element)
-  }
-
-  openApplication () {
+    this.windows.push(new Window(this.idCount++, name, content, this.zCount++, this.desktopElement))
   }
 }
--- src/js/window.js
+++ src/js/window.js
@@ -1,21 +1,76 @@
+import Point2D from './point2d.js'
+
 /**
  * @author Kevin Cederholm
  * @version 1.0
  */
 
 export default class Window {
-  constructor (id, name, content, zIndex) {
+  constructor (id, name, content, zIndex, deskElement) {
     this.element = document.querySelector('#windowtemp').content.cloneNode(true)
-    this.element.querySelector('.window').id = 'window' + id
-    this.name = name
     this.id = 'win' + id
+    this.name = name
     this.zIndex = zIndex
+    this.offset = new Point2D(0, 0)
     this.name = name
     this.content = content
+    this.mouseUpRef = this.mouseUp.bind(this)
+    this.mouseMoveRef = this.onDragMouse.bind(this)
+    this.init()
+    deskElement.append(this.element)
+  }
+
+  init () {
+    this.element.querySelector('.window').id = this.id
+    this.element.querySelector('.window-content').innerHTML = this.content
     this.element.querySelector('.window-name').textContent = this.name
-    // this.element.querySelector('.window-topbar').textContent = this.name + ' Drag ' + ' ExitButton '
     this.element.querySelector('.window').style.top = '100px'
     this.element.querySelector('.window').style.left = '100px'
-    this.element.querySelector('.window').style.zIndex = zIndex
+    this.element.querySelector('.window').style.zIndex = this.zIndex
+    this.element = this.element.querySelector('#' + this.id)
+
+    // test stuff
+    this.element.querySelector('.window-menu').addEventListener('click', event => {
+      // this.moveWindowTo(300, 300)
+    })
+
+    // Move window
+    this.element.querySelector('.window-topbar').addEventListener('mousedown', this.onMouseDown.bind(this))
+    // Close Window
+    this.element.querySelector('.closebtn').addEventListener('click', this.destroy.bind(this))
+  }
+
+  onMouseDown (event) {
+    this.offset.x = (event.clientX - parseInt(event.target.closest('#' + this.id).style.left, 10))
+    this.offset.y = (event.clientY - parseInt(event.target.closest('#' + this.id).style.top, 10))
+
+    document.addEventListener('mousemove', this.mouseMoveRef)
+    document.addEventListener('mouseup', this.mouseUpRef)
+  }
+
+  onDragMouse (event) {
+    this.moveWindowTo((event.clientX - this.offset.x), (event.clientY - this.offset.y))
+  }
+
+  mouseUp (event) {
+    document.removeEventListener('mousemove', this.mouseMoveRef)
+    document.removeEventListener('mouseup', this.mouseUpRef)
+  }
+
+  moveWindowTo (x, y) {
+    // Set maximum offscreen
+    let halfWidth = this.element.clientWidth / 2
+    let halfHeigth = this.element.clientWidth / 2
+
+    if (x >= 0 && x <= (window.innerWidth - halfWidth)) {
+      this.element.style.left = x + 'px'
+    }
+    if (y >= 0 && y <= (window.innerHeight - halfHeigth)) {
+      this.element.style.top = y + 'px'
+    }
+  }
+
+  destroy () {
+    this.element.remove()
   }
 }
