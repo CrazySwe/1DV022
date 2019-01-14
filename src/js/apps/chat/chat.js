a87dcba4f5d879f01003fc2b9f5a9f0103b91aa8
Kevin C <crazy_swe_@hotmail.com>
2019-01-14 10:12:21 +0100

    Adding the ability to change and save username for chat

--- src/js/apps/chat/chat.js
+++ src/js/apps/chat/chat.js
@@ -9,15 +9,23 @@ export default class Chat extends Window {
     super(id, 'ChatApp', zIndex, deskElement, position)
     this.wSocket = new window.WebSocket('ws://vhost3.lnu.se:20080/socket/')
     this.APIKey = 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
-    this.username = 'MyFancyUsername'
-    this.openSocket()
 
+    this.storageName = 'ChatAppUsername'
+
+    this.saveUsernameHandler = this.saveUsername.bind(this)
+    this.openUsernameDialogHandler = this.openUsernameDialog.bind(this)
     this.chatTemplate = document.querySelector('#chatAppTemp').content.cloneNode(true)
     this.contentNode.innerHTML = ''
+    this.addMenu()
+    this.username = this.getUsername()
+    this.chatTemplate.querySelector('.chatusername').innerText = this.username + ':'
     this.contentNode.append(this.chatTemplate)
 
     this.sendMsgHandler = this.sendMessage.bind(this)
     this.contentNode.querySelector('.messageSendBtn').addEventListener('click', this.sendMsgHandler)
+
+    // Open and start the chat
+    this.openSocket()
   }
   openSocket () {
     this.wSocket.onopen = this.onOpen.bind(this)
@@ -55,7 +63,7 @@ export default class Chat extends Window {
     if (msg.type === 'notification') {
       message.style.backgroundColor = 'lightyellow'
     }
-    if (msg.username === 'MyFancyUsername') {
+    if (msg.username === this.username) {
       message.style.backgroundColor = 'lightblue'
     }
     if (msg.channel) {
@@ -80,6 +88,51 @@ export default class Chat extends Window {
       }
       this.wSocket.send(JSON.stringify(msg))
     }
+  }
+
+  addMenu () {
+    let menu = document.createElement('div')
+    let button = document.createElement('a')
+
+    menu.classList.add('window-menu')
+    button.classList.add('menubtn1')
+    button.href = '#'
+    button.innerText = 'Set Username'
+    button.addEventListener('click', this.openUsernameDialogHandler)
+
+    menu.append(button)
+    this.element.insertBefore(menu, this.contentNode)
+  }
+
+  getUsername () {
+    let name = window.localStorage.getItem(this.storageName)
+    if (name === null) {
+      this.openUsernameDialog()
+      name = 'DefaultUserName'
+    }
+    return name
+  }
+
+  saveUsername () {
+    let name = this.contentNode.querySelector('#setUsernameTxt').value
+
+    if (name !== '') {
+      this.username = name
+      window.localStorage.setItem(this.storageName, name)
+      this.contentNode.querySelector('.chatusername').innerText = name + ':'
+
+      this.contentNode.querySelector('.saveUsernameBtn').removeEventListener('click', this.saveUsernameHandler)
+      this.contentNode.querySelector('.overlay').remove()
+      this.contentNode.querySelector('#chatAppSetUsernameDialog').remove()
+    }
+  }
+
+  openUsernameDialog () {
+    // Open dialog for saving username
+    let dialog = document.querySelector('#chatAppSetUserNameTemp').content.cloneNode(true)
+    dialog.querySelector('.saveUsernameBtn').addEventListener('click', this.saveUsernameHandler)
+
+    this.contentNode.append(dialog)
   }
 
   destroy () {
